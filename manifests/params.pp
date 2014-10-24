b886d6c2dc5132585cf001ca1efb33950bb01d76
Edmond Negado <enegado@rockstar.ucsd.edu>
2014-10-24 14:56:04 -0700

    debugging.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,30 +5,11 @@ class openldap::params
   ## place default vars here.
   $uri_list                = []        ## required
   $base                    = undef     ## required
-  $tls_cacert              = undef
-  $tls_reqcert             = undef
-  $tls_cacertdir           = undef
 
-  $base_pkg_name           = undef
-  $client_pkg_name         = undef
-  $server_pkg_name         = undef
-
-  $base_ldap_path          = undef
-  $client_ldap_cfg         = 'ldap.conf'
 
 
   ## nslcd configuration and packages
-  $nscd_pkg_name           = 'nscd'
-  $nslcd_ldap_cfg          = undef
   $nslcd_rootpwmoddn       = undef
-  $nslcd_bind_timelimit    = 30
-  $nslcd_timelimit         = 30
-  $nslcd_idle_timelimit    = 3600
-  $nslcd_uid               = undef
-  $nslcd_gid               = undef
-  $nslcd_ssl               = 'no'
-  $nslcd_tls_cacertdir     = undef
-  $nslcd_tls_reqcert       = undef
 
 
   ## Based on the operating system, set the correct params of where
@@ -41,6 +22,7 @@ class openldap::params
       $client_pkg_name     = 'ldap-auth-client'
       $server_pkg_name     = undef
       $base_ldap_path      = undef
+      $nscd_pkg_name       = 'nscd'
 
     } ## End Debian/Ubuntu case
 
@@ -49,11 +31,17 @@ class openldap::params
       $base_pkg_name       = 'openldap'
       $client_pkg_name     = 'openldap-clients'
       $server_pkg_name     = 'openldap-servers'
+      $client_ldap_cfg         = 'ldap.conf'
       $base_ldap_path      = '/etc/openldap'
       $tls_cacert          = '/etc/pki/tls/certs/ca-bundle.crt'
       $tls_cacertdir       = '/etc/openldap/cacerts'
       $tls_reqcert         = 'demand'
+      $nscd_pkg_name       = 'nscd'
       $nslcd_ldap_cfg      = 'nslcd.conf'
+      $nslcd_bind_timelimit    = 30
+      $nslcd_timelimit         = 30
+      $nslcd_idle_timelimit    = 3600
+      $nslcd_ssl           = 'no'
       $nslcd_uid           = 'nslcd'
       $nslcd_gid           = 'ldap'
       $nslcd_tls_cacertdir = '/etc/openldap/cacerts'
